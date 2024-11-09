15c6f328de176894af38afc0619b94a7113094d9
Gurkirat Singh Chumber <chumbgur@sheridancollege.ca>
2024-11-09 01:25:55 -0500

    transaction gateway

---
+++ backend/gateway/src/controller/transactionController.js
@@ -0,0 +1,75 @@
+const {
+  getUserTransactions,
+  addUserTransaction,
+  updateUserTransaction,
+  deleteUserTransaction,
+} = require("../services/transactionService.js");
+const { getUserId } = require("../services/userService.js");
+
+const getTransactions = async (req, res) => {
+  try {
+    const token = req.signedCookies.access_token;
+    const userId = await getUserId(token);
+    const transactions = await getUserTransactions(userId);
+
+    res.status(200).json({ transactions });
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const addTransaction = async (req, res) => {
+  try {
+    const transactions = req.body;
+    const token = req.signedCookies.access_token;
+    const userId = getUserId(token);
+
+    transactions.forEach((transaction) => {
+      transaction.userId = userId;
+    });
+
+    await addUserTransaction(transactions);
+
+    res.status(201).json({ message: "Added transaction" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const updateTransaction = async (req, res) => {
+  try {
+    const transaction = req.body;
+    const transactionId = req.params.id;
+    const token = req.signedCookies.access_token;
+    const userId = getUserId(token);
+    transaction.userId = userId;
+    await updateUserTransaction(transaction, transactionId);
+    res.status(204).json({ message: "updated transaction" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
+};
+
+const deleteTransaction = async (req, res) => {
+  try {
+    const token = req.signedCookies.access_token;
+    const userId = getUserId(token);
+    const transactionId = req.params.id;
+
+    await deleteUserTransaction(transactionId, userId);
+    res.status(204).json({ message: "Deleted transaction" });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  addTransaction,
+  getTransactions,
+  updateTransaction,
+  deleteTransaction,
+};
--- backend/gateway/src/routes/gatewayRoutes.js
+++ backend/gateway/src/routes/gatewayRoutes.js
@@ -9,16 +9,28 @@ const {
   getAllBudgets,
 } = require("../controller/budgetController.js");
 
+const {
+  getTransactions,
+  addTransaction,
+  updateTransaction,
+  deleteTransaction,
+} = require("../controller/transactionController.js");
+
+//auth routes
 router.post("/login", login);
 router.post("/register", register);
 
+//budget routes
 router.get("/budgets", getAllBudgets);
 router.get("/budgets/:budget_id", getBudget);
 router.put("/budgets/:budget_id", updateBudget);
 router.delete("/budgets/:budget_id", deleteBudget);
 router.post("/budgets", createBudget);
 
-//google auth
-
+//transactions routes
+router.get("/transaction", getTransactions);
+router.post("/transaction", addTransaction);
+router.put("/transaction/:id", updateTransaction);
+router.delete("/transaction/:id", deleteTransaction);
 
 module.exports = router;
